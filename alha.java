d5a5abdb1964f35bf3ed713d2e26a939d8c638bf
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 14:40:13 +0530

    Update alha.java

--- alha.java
+++ alha.java
@@ -1,12 +1,19 @@
 class Ideone
+{
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner s= new Scanner(System.in);
-		System.out.println("enter character");
-		char c = s.next().charAt(0);
-		if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
-		System.out.println("c=is an alhabet");
-		else
-		System.out.println("c is not an alphabet");
+	int a;
+	Scanner obj=new Scanner(System.in);
+	a=obj.nextInt();
+	if(a%4==0)
+	{
+		System.out.println("yes");
+
+	}
+	else
+	{
+		System.out.println("n0");
 		
-		}
+	}
+	}
+}
