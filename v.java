c504344d6bb161cb3435b452502c8e65e24683ee
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 12:08:03 +0530

    Create v.java

---
+++ v.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+class Ideone
+	public static void main (String[] args) throws java.lang.Exception
+	
+	{
+		char ch = 'e';
+	{
+	if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	System.out.println("vowel");
+	else if(ch>='a'&&ch<='z')
+	System.out.println("consonant");
+	else
+	System.out.println("invalid");
+	}
+}
+}
