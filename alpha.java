fdb82a8df620bb314e2dc934b258dcc5aa981cd9
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 12:23:55 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,14 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			char c='e';
+	if ((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+	System.out.println("alaphabet");
+	else
+	System.out.println("no");
+	// your code goes here
+	}
+}
+	
