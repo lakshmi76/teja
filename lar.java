cd98750498998d51c516481254853ec9aca6500f
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 12:50:14 +0530

    Create lar.java

---
+++ lar.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b,c;
+	System.out.println("enter three integers");
+	Scanner in = new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		c==in.nextInt();
+		if( a>b &&a>c)
+		System.out.println(a);
+		if( b>a&&b>c)
+		System.out.println(b);
+		if( c>a&&c>b)
+		System.out.println(c);
+		else
+		System.out.println("entered values are not distinct");
+			}
+     }
+
+ 
